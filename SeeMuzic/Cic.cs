18e1ed8cb0849b29aa01c917a519984cfacca0b9
sma <m.samodov@poisk-it.ru>
2017-04-23 14:26:09 +0300

    Визуализация музыки

---
+++ SeeMuzic/Cic.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace audio_sma
+{
+	class Cic
+	{
+		int Cnt, Shift;
+		public int X, Y;
+		int RRR, NNN, NNN2, MMM;
+		int [] XXX;
+		int [] YYY;
+
+		public Cic (int rrr, int nnn) // rrr - кратность, nnn - стадий
+		{
+			RRR = rrr;
+			NNN = nnn;
+			NNN2 = NNN * 2;
+			MMM = 1;
+			XXX = new int [NNN2];
+			YYY = new int [NNN2];
+			double Gain = Math.Pow ((double)(RRR * MMM), (double)NNN);
+			double Bout = (double)NNN * Math.Log ((double)(RRR * MMM)) / Math.Log (2.0); //+ Bin
+			Shift = (int)(Bout + 0.49999);
+			double Gnorm = Gain / Math.Pow (2.0, Math.Floor (Bout + 0.49999));
+			double Scale = 1.0 / Gnorm;
+			Reset ();
+		}
+
+		public void Reset ()
+		{
+			Cnt = 0;
+			for (int i = 0; i < NNN2; i++)
+			{
+				XXX [i] = YYY [i] = 0;
+			}
+		}
+
+		public bool Decimate (int x, int y)
+		{
+			int i;
+			for (i = 0; i < NNN; i++)
+			{
+				x = (XXX [i] += x);
+				y = (YYY [i] += y);
+			}
+
+			if ((Cnt = (Cnt + 1) % RRR) == 0)
+			{
+				int a, b;
+				for (; i < NNN2; i++)
+				{
+					a = x - XXX [i]; XXX [i] = x; x = a;
+					b = y - YYY [i]; YYY [i] = y; y = b;
+				}
+				X = x >> Shift;
+				Y = y >> Shift;
+				return true;
+			}
+			return false;
+		}
+	}
+
+}
+
+/*
+
+	function Comb1 (S : access Long_Float; X : Long_Float) return Long_Float is
+		E : Long_Float := X - S.all;
+	begin
+		S.all := X;
+		return E;
+	end Comb1;
+
+	procedure Decimate (X : Float) is
+		E : Long_Float := Long_Float (X);
+	begin
+		for I in 1 .. NNN loop
+			E := Inter (A (I)'Access, E);
+		end loop;
+		Cnt := (Cnt + 1) mod RRR;
+		if Cnt = 0 then
+			for I in 1 .. NNN loop
+				E := Comb1 (B (I)'Access, E);
+			end loop;
+			Res (Float (E / Long_Float (Gain)));
+		end if;
+	end Decimate;
+ 
+const int RRR = 16; 
+const int NNN = 4;
+const int NNN2 = NNN * 2;
+const int MMM = 1;
+
+struct CICFLT
+{
+	int Cnt;
+	int Shift;
+	int XXX [NNN2];
+	int YYY [NNN2];
+};
+
+double Bin = 16.0;
+double Gain;
+double Bout;
+double Gnorm;
+double Scale;
+
+CFile Fout;
+
+void Calc_Parm (CICFLT *cicflt)
+{
+	Gain = pow (double (RRR * MMM), double (NNN));
+	Bout = //Bin +
+		double (NNN) * log (double (RRR * MMM)) / log (2.0);
+	Gnorm = Gain / pow (2.0, floor (Bout + 0.49999));
+	Scale = 1.0 / Gnorm;
+	cicflt->Shift = int (Bout + 0.49999);
+//
+//	if 32.0 < Bin + Bout then
+//		raise Exp with "32.0 < Bout for Long_Float !!!";
+//	end if;
+//
+	_tprintf (_T("%f : Gain\n"), Gain);
+	_tprintf (_T("%f : Bout\n"), Bout);
+	_tprintf (_T("%f : Gnorm\n"), Gnorm);
+	_tprintf (_T("%f : Scale\n"), Scale);
+}
+
+void Reset (CICFLT *cicflt)
+{
+	cicflt->Cnt = 0;
+	for (int i = 0; i < NNN2; i++)
+		cicflt->XXX [i] = cicflt->YYY [i] = 0;
+}
+
+void Decimate (CICFLT *cicflt, int X, int Y)
+{
+	int i;
+	for (i = 0; i < NNN; i++)
+	{
+		X = (cicflt->XXX [i] += X);
+		Y = (cicflt->YYY [i] += Y);
+	}
+
+	if (RRR <= ++cicflt->Cnt)
+	{
+		int e;
+		cicflt->Cnt = 0;
+		for (; i < NNN2; i++)
+		{
+			e = X - cicflt->XXX [i]; cicflt->XXX [i] = X; X = e;
+			e = Y - cicflt->YYY [i]; cicflt->YYY [i] = Y; Y = e;
+		}
+		X >>= cicflt->Shift;
+		Y >>= cicflt->Shift;
+		//_tprintf (_T("%d\t%d\n"), X, Y);
+		Fout.Write (&X, sizeof (INT16));
+		Fout.Write (&Y, sizeof (INT16));
+	}
+}
+*/
---
+++ SeeMuzic/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Un4seen.Bass;
+using audio_sma;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace SeeMuzic
+{
+	public partial class Form1 : Form
+	{
+		const string fname = "Зодиак - Космическая музыка.mp3";
+		const int TIMER_INTERVAL_MS = 40; // 1000 / 40 = 25 кадров в сек.
+		const int CICKRAT = 14; // (44100 / 14 = 3150) / 25 = 126
+
+		static int stream = 0;
+		static int len = 0;
+		static short [] buf;
+		static int [] Xbuf, Ybuf;
+		static audio_sma.Cic cic = new Cic (CICKRAT, 4);
+		static int ibuf = 0;
+		//static BinaryFormatter formatter = new BinaryFormatter ();
+		//static FileStream fs = new FileStream ("test.pcm", FileMode.Append);
+		BinaryWriter bw = new BinaryWriter (File.Open ("test.pcm", FileMode.Create));
+
+		public Form1 ()
+		{
+			InitializeComponent ();
+		}
+		// Form1
+
+		private void Form1_Load (object sender, EventArgs e)
+		{
+			if (!Bass.BASS_Init (-1, 44100, BASSInit.BASS_DEVICE_DEFAULT | BASSInit.BASS_DEVICE_FREQ, IntPtr.Zero))
+			{
+				MessageBox.Show (String.Format ("Stream error: {0}", Bass.BASS_ErrorGetCode ()), "Error");
+				this.Close ();
+			}
+
+			stream = Bass.BASS_StreamCreateFile (fname, 0, 0, BASSFlag.BASS_DEFAULT);
+			if (stream == 0)
+			{
+				MessageBox.Show (String.Format ("Stream error: {0}", Bass.BASS_ErrorGetCode ()), "Error");
+				this.Close ();
+			}
+
+			len = (int)Bass.BASS_ChannelSeconds2Bytes (stream, TIMER_INTERVAL_MS / 1000.0);
+			//MessageBox.Show (String.Format ("len = {0}\nlen4 = {1}\nlencic = {2}", len, len / 4, len / 4 / CICKRAT));
+			buf = new short [len];
+			Xbuf = new int [len / 4 / CICKRAT + 1];
+			Ybuf = new int [len / 4 / CICKRAT + 1];
+
+			Bass.BASS_ChannelPlay (stream, false);
+
+			timer1.Interval = TIMER_INTERVAL_MS;
+			timer1.Enabled = true;
+		}
+		// Form1_Load
+
+		private void Form1_FormClosed (object sender, FormClosedEventArgs e)
+		{
+			Bass.BASS_StreamFree (stream);
+			Bass.BASS_Free ();
+		}
+		// Form1_FormClosed
+
+		private void timer1_Tick (object sender, EventArgs e)
+		{
+//
+			//int len4 = Bass.BASS_ChannelGetData (stream, buf, len);
+			//bw.Write ((short)len4);
+			//int nnn = len4 / 2 / CICKRAT;
+			//byte [] bbb = new byte [nnn];
+			//for (int i = 0, j = 0; i < len4 / 4; i += 2)
+			//{
+			//	if (cic.Decimate ((int)buf [i], (int)buf [i + 1]))
+			//	{
+			//		if (j <= nnn - 4)
+			//		{
+			//			bbb [j++] = (byte)cic.X;
+			//			bbb [j++] = (byte)(cic.X >> 8);
+			//			bbb [j++] = (byte)cic.Y;
+			//			bbb [j++] = (byte)(cic.Y >> 8);
+			//		}
+			//	}
+			//}
+			//bw.Write (bbb);
+			//formatter.Serialize (fs, test);
+			
+			ibuf = 0;
+			int len4 = Bass.BASS_ChannelGetData (stream, buf, len) / 4;
+			for (int i = 0; i < len4; i += 2)
+			{
+				if (cic.Decimate ((int)buf [i], (int)buf [i + 1]))
+				{
+					Xbuf [ibuf] = cic.X;
+					Ybuf [ibuf] = cic.Y;
+					ibuf++;
+				}
+			}
+			Invalidate ();
+		}
+		// timer1_Tick
+
+		const int OKNO = 64;
+
+		private void Form1_Paint (object sender, PaintEventArgs e)
+		{
+			if (ibuf <= 0) return;
+
+			int OKNO = ibuf;
+			Graphics g = e.Graphics;
+			Bitmap bmp1 = new Bitmap (OKNO, OKNO, g);
+			for (int x = 0; x < OKNO; x++)
+			{
+				for (int y = 0; y < OKNO; y++)
+				{
+					//int eee = (Xbuf [x] + Ybuf [y]) / 64;
+					//if (0 < eee)
+					//{
+					//	bmp1.SetPixel (x, OKNO - y - 1, Color.FromArgb (0, eee & 255, 0));
+					//}
+					//else
+					//{
+					//	bmp1.SetPixel (x, OKNO - y - 1, Color.FromArgb (-eee & 255, 0, 0));
+					//}
+					int eee = (int)(Math.Abs ((Xbuf [x] + Ybuf [y]) / 64));
+					//int eee = (int)((Math.Abs (Xbuf [x]) + Math.Abs (Ybuf [y])) / 64);
+					if (eee < 256)
+						bmp1.SetPixel (x, OKNO - y - 1, Color.FromArgb (0, 0, eee));
+					else if (eee < 512)
+						bmp1.SetPixel (x, OKNO - y - 1, Color.FromArgb (eee - 256, 0, 255));
+					else if (eee < 512 + 256)
+						bmp1.SetPixel (x, OKNO - y - 1, Color.FromArgb (255, eee - 512, 255));
+					else
+						bmp1.SetPixel (x, OKNO - y - 1, Color.FromArgb (255, 255, 255));
+				}
+			}
+			Image img1 = bmp1;
+			//g.DrawImage (img1, 0, 0, OKNO, OKNO);
+			g.DrawImage (img1, 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+		}
+		// Form1_Paint
+
+	}
+}
